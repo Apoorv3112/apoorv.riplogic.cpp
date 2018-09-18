811fc2a2ee7e2df3b8cf65bc9f3353850787b95e
Apoorv Chandrawanshi <apoorv3112@hotmail.com>
2018-09-18 13:53:39 +0530

    Update ELLIPSE.cpp

--- ELLIPSE.cpp
+++ ELLIPSE.cpp
@@ -16,10 +16,10 @@ int quad(long double &ra,long double &rb,long double &rc)
   cout<<"the roots are x="<<e<<"x="<<i << endl;
   cout<<"___________________________________________________";
 }
-int delt()
+int ellipse()
 {
 	int a,b,c,h,f,g,delta,abh,e;
-	long double C,A,B,H,sma,smi;
+	long double C,A,B,H,sma,smi,tan1,tan2,tana,tanb,tanc,d1,e1,i1,ra,rb,rc,r1,r2,d2,e2,i2,a1,b1,m1,m2;
 	cout<<".########.##.......##.......####.########...######..######## \n"
           ".##.......##.......##........##..##.....##.##....##.##...... \n"
           ".##.......##.......##........##..##.....##.##.......##...... \n"
@@ -47,11 +47,33 @@ int delt()
     cout<<"B="<<B<<'\n';
     cout<<"H="<<H<<'\n';
     e=pow((2*pow(pow(A-B,2)+4*pow(H,2),1/2))/((A-B)+(pow(A-B,2)+4*pow(H,2),1/2)),1/2);
-    cout<<"eccentricity is:"<<e;    
+    cout<<"eccentricity is:"<<e<<'\n'; 
+	tana=2*H;
+	tanb=2*(A+B);
+	tanc=-2*H; 
+    d1=sqrt(tanb*tanb-4*tana*tanc);
+    e1=-(tanb+d1)/2*tana;
+    i1=-(-tanb-d1)/2*tana;
+    cout<<"The two values of tan(theta) are:"<<e1<<"&"<<i1<<'\n';
+    ra=1;
+    rb=-(A+B);
+    rc=A*B -pow(H,2);
+    d2=sqrt(rb*rb-4*ra*rc);
+    e2=-(rb+d2)/2*ra;
+    i2=-(-rb-d2)/2*ra;
+    a1=1/(pow(e2,2));
+    b1=1/(pow(i2,2));
+    m1=pow((1+pow(e1,2))/(A+2*H*e1+B*pow(e1,2)),1/2);
+    m2=pow((1+pow(i1,2))/(A+2*H*i1+B*pow(i1,2)),1/2);
+    if(e2==m1)
+	{
+	cout<<"The major/minor axis with its tan(theta)are:"<<'\n'<<"a="<<e2<<"& m1="<<e1<<'\n'<<"b="<<i2<<"& ="<<i2<<'\n';
+	}
+	else{cout<<"The major/minor axis with its tan(theta)are:"<<'\n'<<"a="<<e2<<"& m1="<<i1<<'\n'<<"b="<<i2<<"& ="<<e1<<'\n';}
 }
 
 int main()
 {
 
-	delt();
+	ellipse();
 }
